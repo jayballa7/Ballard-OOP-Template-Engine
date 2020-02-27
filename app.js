78c5b849abcff5809773ecf75dd82e08da03b3d0
jayballa7 <Jenn_Ballard7@hotmail.com>
2020-02-26 23:26:34 -0800

    tests all pass!

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const Manager = require("./Libs/Manager");
 const Engineer = require("./Libs/Engineer");
+const Intern = require("./Libs/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
